98f07eec2b749fbe344cc2bb0e12623c4f75d349
babababasb <3185566426@qq.com>
2026-06-27 06:02:37 +0800

    auto: 2026-06-27 06:02:37

--- app/src/main/java/com/example/globalpeq/AudioProcessingPolicy.java
+++ app/src/main/java/com/example/globalpeq/AudioProcessingPolicy.java
@@ -55,7 +55,7 @@ final class AudioProcessingPolicy {
         }
         Preset effective = preset.withReverbType("Default");
         if (!systemBassBoostAllowed(bassModeIndex)) {
-            effective = effective.withSystemBassBoostPercent(0);
+            effective = effective.withBassEnhanceAmountPercent(0);
         }
         return effective;
     }
