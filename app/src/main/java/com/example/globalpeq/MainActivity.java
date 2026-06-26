3d20c6f7dd63126be3b5523fb2bd0bb797cb5961
babababasb <3185566426@qq.com>
2026-06-26 20:46:41 +0800

    auto: 2026-06-26 20:46:41

--- app/src/main/java/com/example/globalpeq/MainActivity.java
+++ app/src/main/java/com/example/globalpeq/MainActivity.java
@@ -456,6 +456,12 @@ public final class MainActivity extends Activity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        renderAll();
+    }
+
+    @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (!hasFocus) return;
