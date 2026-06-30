497e0575c4614391193e8be517b1ca583ed52d33
WanAn522z <3185566426@qq.com>
2026-07-01 01:57:46 +0800

    auto snapshot 1161 2026-07-01 01:57:46

--- app/src/main/java/com/example/globalpeq/MainActivity.java
+++ app/src/main/java/com/example/globalpeq/MainActivity.java
@@ -264,9 +264,6 @@ public final class MainActivity extends Activity {
     private TextView shizukuRuntimeReplayView;
     private TextView shizukuAccessButton;
     private TextView shizukuAccessStatusView;
-    private View shizukuReplayFallbackRowView;
-    private TextView shizukuReplayFallbackLabelView;
-    private TextView shizukuReplayFallbackHintView;
     private TextView advancedModeSummaryView;
     private TextView languageLabelView;
     private TextView languageButton;
